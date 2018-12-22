062f02720c6e73f8d8a50a092cd8e1e4f25fce51
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-12-22 21:11:10 +0000

    reading and writing files using built in node methods

--- app.js
+++ app.js
@@ -1,34 +1,17 @@
-var events = require('events');
-var util = require('util');
+// fetches built in Node method 'fs'
+var fs = require('fs');
 
-    // example 1
-// var myEmitter = new events.EventEmitter();
+// synconous version of method
+// code below will not fire until this event (syncronous event)
+//  fs module has gone out and read the contents of the txt file
+var readMe = fs.readFileSync('readMe.txt', 'utf8');
 
-// myEmitter.on('someEvent', function(mssg){
-//     console.log(mssg);
+//uses a second node method to get create a new file to display the text from the readMe file in
+fs.writeFileSync('writeMe.txt', readMe);
+
+// // asyncronous versions of these methods
+// file si written right away even though it is later in the code i.e. asyncronous
+// fs.readFile('readMe.txt', 'utf8', function(err, data){
+//         fs.writeFile('writeMe.txt', data);
 // });
-
-// myEmitter.emit('someEvent', 'the event was emitted');
-
-    // example 2
-var Person = function(name){
-    this.name = name;
-};
-
-util.inherits(Person, events.EventEmitter);
-
-var james = new Person('james');
-var mary = new Person('mary');
-var ryu = new Person('ryu');
-var people = [james, mary, ryu];
-
-people.forEach(function(person) {
-   person.on('speak', function(mssg){
-       console.log(person.name + ' said: ' + mssg);
-   }); 
-});
-
-james.emit('speak', 'hey dudes');
-ryu.emit('speak', "I want a curry")
-
 
