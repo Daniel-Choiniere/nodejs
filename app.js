4292760550ce7856098fe51252f4fadc70ae447e
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-12-22 20:46:43 +0000

    using event emitters to make custom events

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-// gets the module from the other file
-var stuff = require('./stuff');
+var events = require('events');
+var util = require('util');
 
-console.log(stuff.counter(['shaun', 'crystal', 'ryu']));
-console.log(stuff.adder(5,6));
-console.log(stuff.pi);
-console.log(stuff.adder(stuff.pi, 5));
+    // example 1
+// var myEmitter = new events.EventEmitter();
+
+// myEmitter.on('someEvent', function(mssg){
+//     console.log(mssg);
+// });
+
+// myEmitter.emit('someEvent', 'the event was emitted');
+
+    // example 2
+var Person = function(name){
+    this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person('james');
+var mary = new Person('mary');
+var ryu = new Person('ryu');
+var people = [james, mary, ryu];
+
+people.forEach(function(person) {
+   person.on('speak', function(mssg){
+       console.log(person.name + ' said: ' + mssg);
+   }); 
+});
+
+james.emit('speak', 'hey dudes');
+ryu.emit('speak', "I want a curry")
+
+
