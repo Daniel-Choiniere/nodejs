ee5be431392f36990ccb260609a79b81b303acf0
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-12-22 21:29:20 +0000

    creating directories and files asyncronous and syncronous methods

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-// fetches built in Node method 'fs'
 var fs = require('fs');
 
-// synconous version of method
-// code below will not fire until this event (syncronous event)
-//  fs module has gone out and read the contents of the txt file
-var readMe = fs.readFileSync('readMe.txt', 'utf8');
+        // delete a file using node 'fs.unlink' method
+// fs.unlink('writeMe.txt');
+        
+        // asyncronous versions of creating/deleting directories
+        // create a directory using node 'fs.mkdirSync' method
+// fs.mkdirSync('stuff');
 
-//uses a second node method to get create a new file to display the text from the readMe file in
-fs.writeFileSync('writeMe.txt', readMe);
+        // deletes a directory using node 'rmdirSync' method
+// fs.rmdirSync('stuff');
 
-// // asyncronous versions of these methods
-// file si written right away even though it is later in the code i.e. asyncronous
-// fs.readFile('readMe.txt', 'utf8', function(err, data){
-//         fs.writeFile('writeMe.txt', data);
+
+        // syncronous versions of creating/deleting directories
+        // reads a file, and creates a new directory and a file in that new directory to store the read text
+// fs.mkdir('stuff', function(){
+//   fs.readFile('readMe.txt', 'utf8', function(err, data){
+//       fs.writeFile('./stuff/writeMe.txt', data); 
+//   }); 
 // });
+
+        // remove a file and its parent direcotory
+// fs.unlink('./stuff/writeMe.txt', function(){
+//     fs.rmdir('stuff');
+// })
+
+
+
+
+
+
 
