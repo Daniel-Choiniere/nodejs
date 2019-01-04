5f1c34dd1fad729e903bae80ab6ad9a0162bc66f
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-04 05:12:21 +0000

    created readable stream

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var http = require("http");
 
-var server = http.createServer(function(req, res){
-        console.log('request was made ' + req.url);
-        // create response headers
-        res.writeHead(200, {'Content-Type' : 'text/plain'});
-        res.end('Hey ninjago');
+var fs = require('fs');
+
+var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+
+myReadStream.on('data', function(chunk){
+   console.log('new cdhunk received:');
+   console.log(chunk);
 });
 
-server.listen(process.env.PORT, process.env.IP, function() {
-    console.log('yo dawgs, now lstening to port 8080');
-});
+// var server = http.createServer(function(req, res){
+//         console.log('request was made ' + req.url);
+//         // create response headers
+//         res.writeHead(200, {'Content-Type' : 'text/plain'});
+//         res.end('Hey ninjago');
+// });
+
+// server.listen(process.env.PORT, process.env.IP, function() {
+//     console.log('yo dawgs, now lstening to port 8080');
+// });
