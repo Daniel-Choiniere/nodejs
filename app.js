6b5d449d5e8ba1dabdb8d7ec793da7c0d9496e65
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-05 17:56:43 +0000

    use pipe to read data bit by bit and get it ot the user faster

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 var http = require("http");
-
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
-
-myReadStream.on('data', function(chunk){
-   console.log('new cdhunk received:');
-   console.log(chunk);
+var server = http.createServer(function(req, res){
+        console.log('request was made ' + req.url);
+        // create response headers
+        res.writeHead(200, {'Content-Type' : 'text/plain'});
+        var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+        myReadStream.pipe(res);
 });
 
-// var server = http.createServer(function(req, res){
-//         console.log('request was made ' + req.url);
-//         // create response headers
-//         res.writeHead(200, {'Content-Type' : 'text/plain'});
-//         res.end('Hey ninjago');
-// });
-
-// server.listen(process.env.PORT, process.env.IP, function() {
-//     console.log('yo dawgs, now lstening to port 8080');
-// });
+server.listen(process.env.PORT, process.env.IP, function() {
+    console.log('yo dawgs, now lstening to port 8080');
+});
