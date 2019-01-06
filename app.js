ef0d8ebea445e913ce75f2e124bc01cfaa87da18
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-06 13:33:58 +0000

    introduction to express, greated two GET routes which send data to different webpage routes

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
-var http = require("http");
-var fs = require('fs');
+var express = require("express");
 
-var server = http.createServer(function(req, res){
-        console.log('request was made ' + req.url);
-        if (req.url === '/home' || req.url === '/') {
-                res.writeHead(200, {'Content-Type' : 'text/html'});
-                fs.createReadStream(__dirname + '/index.html').pipe(res);
-        } else if (req.url === '/contact'){
-                res.writeHead(200, {'Content-Type' : 'text/html'});
-                fs.createReadStream(__dirname + '/contacts.html').pipe(res);
-        } else if (req.url === '/api/ninjas') {
-                var ninjas = [{name: 'ryu', age: 29}, {name : 'yoshi', age: 33}];
-                res.writeHead(200, {'Content-Type' : 'application/JSON'});
-                res.end(JSON.stringify(ninjas));
-        } else {
-                res.writeHead(404, {'content-Type' : 'text/html'});
-                fs.createReadStream(__dirname + '/404.html').pipe(res);
-        }
+var app = express();
+
+app.get('/', function(req, res){
+   res.send('this is the homepage')     
+});
+   
+app.get('/contact', function(req, res){
+   res.send('this is the contactpage');  
 });
 
-server.listen(process.env.PORT, process.env.IP, function() {
-    console.log('yo dawgs, now lstening to port 8080');
-});
+app.listen(process.env.PORT, process.env.IP);
+
