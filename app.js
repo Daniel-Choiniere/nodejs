3af12b9d8eb372f0a1ffd6dc1903c2fd505c59ef
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-06 13:42:05 +0000

    express dynamic express route parameters

--- app.js
+++ app.js
@@ -10,5 +10,8 @@ app.get('/contact', function(req, res){
    res.send('this is the contactpage');  
 });
 
+app.get('/profile/:name', function(req, res){
+        res.send('you requested to see the profile with the name of ' + req.params.name);         
+});
 app.listen(process.env.PORT, process.env.IP);
 
