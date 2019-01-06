6e87d3ce2ed1b31c28f821d8ba91cc9b6f11667c
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-06 03:45:11 +0000

    serving JSON data to the client

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ var fs = require('fs');
 var server = http.createServer(function(req, res){
         console.log('request was made ' + req.url);
         // create response headers
-        res.writeHead(200, {'Content-Type' : 'text/html'});
-        var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
-        myReadStream.pipe(res);
+        res.writeHead(200, {'Content-Type' : 'application/json'});
+        
+        var myObj = {
+                name: 'Ryu',
+                job: 'NInja', 
+                age: 29
+        };
+        res.end(JSON.stringify(myObj));
 });
 
 server.listen(process.env.PORT, process.env.IP, function() {
